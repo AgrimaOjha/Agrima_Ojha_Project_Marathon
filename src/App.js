c98b46cd406de21f6c357f59c1c84bea366f99dd
Agrima Ojha <agrimaojha@gmail.com>
2025-04-14 18:59:01 +0530

    Login component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
+import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          hiiii
-        </p>
+        <Login/>
     </div>
   );
 }
